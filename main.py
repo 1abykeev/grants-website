7c2200f2887f1137e6f1c1f9611612fea5ad82dd
Aktan <aktan.abykeev@gmail.com>
2024-10-19 23:35:13 +0300

    Frontend part is ready

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+
+
 
 
 @app.route('/')
 def get_all_posts():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/info')
+def info():
+    return render_template("info.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/pricing')
+def pricing():
+    return render_template("pricing.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
