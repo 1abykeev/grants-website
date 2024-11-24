753cebed913dba4a0b54e13f881aa4f570eeab69
Aktan <aktan.abykeev@gmail.com>
2024-11-24 22:45:37 +0300

    Skip

--- forms.py
+++ forms.py
@@ -26,9 +26,37 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, IntegerField, URLField, TextAreaField, DecimalField, BooleanField, SelectField
+from wtforms.validators import DataRequired, URL, Email, Regexp, Optional
+from flask_ckeditor import CKEditorField
+
+
+
+# Create a form to register new users
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email format.")])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email format.")])
+    password = PasswordField("Password", validators=[DataRequired()])
+    course_code = StringField(
+        "Course Code",
+        validators=[
+            DataRequired(),
+            Regexp(r'^\d{6}$', message="Please enter a valid 6-digit course code.")
+        ]
+    )
+    submit = SubmitField("Login")
+
+
 class AddUniversityForm(FlaskForm):
     uni_name = StringField("University Name", validators=[DataRequired()], render_kw={"rows": 30, "cols": 80})
-    uni_logo = StringField("Logo PNG Link", validators=[Optional()], render_kw={"rows": 30, "cols": 80})
+    uni_logo = URLField("Logo PNG Link", validators=[Optional()], render_kw={"rows": 30, "cols": 80})
     uni_picture = StringField("University Picture Link", validators=[Optional()], render_kw={"rows": 30, "cols": 80})
     uni_desc = TextAreaField("Description", validators=[DataRequired()], render_kw={"rows": 50, "cols": 150})
     uni_official_page_link = URLField("Link to Official Webpage of University", validators=[Optional()], render_kw={"rows": 30, "cols": 80})
