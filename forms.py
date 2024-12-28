103b164ff949695ecc7e3234fb5f47bdcaf0dd4b
Aktan <aktan.abykeev@gmail.com>
2024-12-28 18:10:04 +0300

    skip

--- forms.py
+++ forms.py
@@ -1,29 +1,29 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, IntegerField, URLField, TextAreaField, DecimalField, BooleanField, SelectField
-from wtforms.validators import DataRequired, URL, Email, Regexp, Optional
-from flask_ckeditor import CKEditorField
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField, PasswordField, IntegerField, URLField, TextAreaField, DecimalField, BooleanField, SelectField
+# from wtforms.validators import DataRequired, URL, Email, Regexp, Optional
+# from flask_ckeditor import CKEditorField
 
 
 
-# Create a form to register new users
-class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email format.")])
-    password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
-    submit = SubmitField("Sign Up")
+# # Create a form to register new users
+# class RegisterForm(FlaskForm):
+#     email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email format.")])
+#     password = PasswordField("Password", validators=[DataRequired()])
+#     name = StringField("Name", validators=[DataRequired()])
+#     submit = SubmitField("Sign Up")
 
 
-class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email format.")])
-    password = PasswordField("Password", validators=[DataRequired()])
-    course_code = StringField(
-        "Course Code",
-        validators=[
-            DataRequired(),
-            Regexp(r'^\d{6}$', message="Please enter a valid 6-digit course code.")
-        ]
-    )
-    submit = SubmitField("Login")
+# class LoginForm(FlaskForm):
+#     email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email format.")])
+#     password = PasswordField("Password", validators=[DataRequired()])
+#     course_code = StringField(
+#         "Course Code",
+#         validators=[
+#             DataRequired(),
+#             Regexp(r'^\d{6}$', message="Please enter a valid 6-digit course code.")
+#         ]
+#     )
+#     submit = SubmitField("Login")
 
 
 from flask_wtf import FlaskForm
